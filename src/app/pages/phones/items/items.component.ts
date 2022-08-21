63333202c099b34ab751a97390ff3475a4b2d544
Mihai Dornescu <dornescu@gmail.com>
2022-08-21 20:45:46 +0300

    restore setup id

--- src/app/pages/phones/items/items.component.ts
+++ src/app/pages/phones/items/items.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from 'src/app/services/api.service';
 import {Route, Router, ActivatedRoute} from '@angular/router';
-import {filter, map} from 'rxjs/operators';
-import {LocalPhonesService} from 'src/app/services/localPhones.service';
 
 
 @Component({
@@ -12,27 +10,17 @@ export class ItemsComponent implements OnInit {
   id = '';
   product: any = [];
 
-  lPhones: any =  [
-    {
-      id: 'phone-1', title: 'phone-1', price: 399
-    },
-    {
-      id: 'phone-2', title: 'phone-2', price: 299
-    },
-    {
-      id: 'phone-3', title: 'phone-4', price: 499
-    }
-  ];
-
-  constructor(private localPhones: LocalPhonesService, private route: ActivatedRoute) {
+  constructor(private apiPhones: ApiService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
-    // this.product =this.route.snapshot.paramMap.get('id');
-    this.lPhones.find(x =>x.id === this.id);
-
-
+    // this.product = this.apiPhones.getProduct().subscribe(item => console.log(item.filter((p: any) => p.id ===this.id)));
+    this.product = this.apiPhones.getProduct().subscribe(item =>{
+      const data =item.filter((product: any) => product.id === this.id);
+      console.log(data);
+      return data;
+    });
     console.log(this.product);
   }
 
